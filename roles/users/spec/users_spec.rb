491ebd9e2213f3eb9d646b4a01a646acf3ea78eb
Philip Cheong <isctsf@gmail.com>
2017-08-08 17:10:32 +0200

    add users_spec.rb tests

---
+++ roles/users/spec/users_spec.rb
@@ -0,0 +1,17 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'spec_helper'
+
+describe "INFO: Testing role users/tasks/main.yml" do
+
+  	describe "INFO: checking user webapp" do
+        describe user('webapp') do
+            it { should belong_to_group 'webapp' }
+            it { should have_login_shell '/bin/bash' }
+            it { should have_home_directory '/webapps/webapp' }
+        end
+  	end
+end
+
+
